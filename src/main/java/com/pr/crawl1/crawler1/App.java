659394c2a87bc4745b212e159691946c3eb9524a
Sai Sanjeev <sanjeev.kolasani@imaginea.com>
2016-02-22 16:02:12 +0530

    created states for class App and formatted downloaded mails

--- src/main/java/com/pr/crawl1/crawler1/App.java
+++ src/main/java/com/pr/crawl1/crawler1/App.java
@@ -1,13 +1,11 @@
 package com.pr.crawl1.crawler1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,76 +14,80 @@ import org.jsoup.select.Elements;
 /**
  * Hello world!
  *
- */class App extends DeleteFiles implements Serializable
- {
-/**
+ */
+class App {
+	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	String from;
+	String subject;
+	String date;
+	String content;
 
-public Document connectUrl(String url)
-{
-	Document doc=null;
-	try
-	{
-		doc=Jsoup.connect(url).get();
+	public Document connectUrl(String url) {
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+		return doc;
 	}
-	catch(IOException ie)
-	{
-		ie.printStackTrace();
-	}
-	return doc;
-}
-public void collectLinks(Document doc,String url) throws IOException
-{
-	int i=0;
-	System.out.println("enter the year");
-	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	String year=br.readLine();
-	DeleteFiles df=new DeleteFiles();
-    df.deleteFiles("/home/sanjeevn/arena/crawler1", ".txt");
-	Elements links=doc.select("a[href]");
-	for(Element link : links)
-	{
-		if(link.toString().contains(year) && link.toString().contains("thread"))
-		{
-		url=url.concat(link.attr("href").toString());
-		Document tempDoc1=connectUrl(url);
-		Elements tempLinks1=tempDoc1.select("a[href]");
-		for(Element tempLink1 : tempLinks1)
-		{
-			if(tempLink1.toString().contains("%"))
-			{
-				url=url.replaceAll("/thread","/raw/");
-				url=url.concat(tempLink1.attr("href").toString());
-				Document tempDoc2=connectUrl(url);
-				String fileName="file"+i+".txt";
-				i++;
-				FileOutputStream fileOut=null;
-				ObjectOutputStream objectOut;
-				try {
-					 fileOut = new FileOutputStream(fileName);
-					objectOut = new ObjectOutputStream(fileOut);
-					objectOut.writeObject(tempDoc2.text());
-					System.out.println("file is created");
-					} 
-			    catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+
+	public void collectLinks(Document doc, String url) throws IOException {
+		System.out.println("enter the year");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String year = br.readLine();
+		DeleteFiles df = new DeleteFiles();
+		df.deleteFiles("/home/sanjeevn/arena/crawler1", ".txt");
+		Elements threadLinks = doc.select("a[href]");
+		for (Element threadLink : threadLinks) {
+			if (threadLink.toString().contains(year)
+					&& threadLink.toString().contains("thread")) {
+				url = url.concat(threadLink.attr("href").toString());
+				Document threadDoc = connectUrl(url);
+				Elements emailLinks = threadDoc.select("a[href]");
+				for (Element emailLink : emailLinks) {
+					if (emailLink.toString().contains("%")) {
+						if (url.contains("thread")) {
+							url = url.replaceAll("/thread", "/ajax/");
+						} else {
+							url = url.replace("/ajax", "/ajax/");
+						}
+						url = url.concat(emailLink.attr("href").toString());
+						Document emailDoc = connectUrl(url);
+						try {
+							from = emailDoc.select("from").text();
+							subject = emailDoc.select("subject").text();
+							date = emailDoc.select("date").text();
+							content = emailDoc.select("contents").text();
+							date = date + ".txt";
+							File file = new File(date);
+							FileWriter fileWriter = new FileWriter(
+									file.getAbsoluteFile());
+							BufferedWriter bw = new BufferedWriter(fileWriter);
+							bw.write("From :" + from);
+							bw.write("\n\nsubject:" + subject);
+							bw.write("\n\ncontents:" + content);
+							bw.close();
+							System.out.println("file is created");
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						url = url.substring(0, 69);
+					}
 				}
-				url=url.substring(0,69);
+				url = url.substring(0, 53);
 			}
 		}
-		url=url.substring(0,53);
-		}
 	}
-}
 
-public static void main(String[] args) throws IOException  {
-	String url="http://mail-archives.apache.org/mod_mbox/maven-users/";
-	App app1=new App();
-	Document actualDoc=app1.connectUrl(url);
-		app1.collectLinks(actualDoc,url);
-	
-}	
+	public static void main(String[] args) throws IOException {
+		String url = "http://mail-archives.apache.org/mod_mbox/maven-users/";
+		App app1 = new App();
+		Document actualDoc = app1.connectUrl(url);
+		app1.collectLinks(actualDoc, url);
+
+	}
 }
